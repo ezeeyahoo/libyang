0f675119c1e1f41784280f068c519a2ed819c7a8
Michal Vasko <mvasko@cesnet.cz>
2020-08-19 12:33:00 +0200

    cmake BUGFIX update tested var

--- CMakeModules/SourceFormat.cmake
+++ CMakeModules/SourceFormat.cmake
@@ -5,7 +5,7 @@ macro(LIBYANG_FORMAT)
     find_program(UNCRUSTIFY uncrustify)
     find_package_handle_standard_args(uncrustify DEFAULT_MSG UNCRUSTIFY)
 
-    if(CLANG_FORMAT)
+    if(UNCRUSTIFY)
         add_custom_target(format
                 COMMAND ${UNCRUSTIFY} -c ${CMAKE_SOURCE_DIR}/uncrustify.cfg --no-backup --replace ${CMAKE_SOURCE_DIR}/src/* ${CMAKE_SOURCE_DIR}/compat/*
                 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
